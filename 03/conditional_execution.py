f81f889619362af7e5d2c6f90728057dfdef9427
Christian MV <chris.mlvz@gmail.com>
2021-02-19 17:49:13 -0600

    Conditional ExecutionPassed

---
+++ 03/conditional_execution.py
@@ -0,0 +1,67 @@
+# x = 5
+# if x < 10:
+#     print('Smaller')
+# if x > 20:
+#     print('Bigger')
+
+# print('Finis')
+
+# * Comparison Operators
+# x = 5
+# if x == 5
+# print('Equal 5')
+# if x > 4
+# print('Greater than 4')
+# if x >= 5
+# print('Greather than or Equals 5')
+# if x < 6:
+#     print('Less than 6')
+# if x <= 5:
+#     print('Less than or Equals 5')
+# if x != 6:
+#     print('Not equal 6')
+
+# * One-Way Decisions
+# x = 5
+# print('Before 5')
+# if x == 5:
+#     print('Is 5')
+#     print('Is still 5')
+#     print('Third 5')
+# print('Afterwards 5')
+# print('Before 6')
+# if x == 6:
+#     print('Is 6')
+#     print('Is Still 6')
+#     print('Third 6')
+# print('Afterwards 6')
+
+# * Indented
+# x = 5
+# if x > 2:
+#     print('Bigger than 2')
+#     print('Still bigger')
+# print('Done with 2')
+
+# for i in range(5):
+#     print(i)
+#     if i > 2:
+#         print('Bigger than 2')
+#     print('Done with i', i)
+# print('All done')
+
+# * Nested Decisions
+# x = 42
+# if x > 1:
+#     print('More than one')
+#     if x < 100:
+#         print('Less than 100')
+# print('All done')
+
+# * Two-way Decisions with else
+x = 4
+if x > 2:
+    print('Bigger')
+else:
+    print('Smaller')
+print('All done')
