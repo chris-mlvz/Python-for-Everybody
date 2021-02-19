7379304ebc6d49f1a84b34edf3d3ca8264270b26
Christian MV <chris.mlvz@gmail.com>
2021-02-19 16:07:55 -0600

    Intermediate ExpressionsPassed

---
+++ intermediate_expressions.py
@@ -0,0 +1,70 @@
+# xx = 2
+# xx = xx + 2
+# print(xx)
+
+# yy = 440 * 12
+# print(yy)
+
+# zz= yy / 1000
+# print(zz)
+
+# jj = 23
+# kk = jj % 5
+# print(kk)
+
+# print(4 ** 3)
+
+# x = 1 + 2 ** 3 / 4 * 5
+# print(x)
+
+# ddd = 1 + 4
+# print(ddd)
+
+# eee = 'hello' + 'there'
+# print(eee)
+
+# # eee = eee + 1
+# print(type(eee))
+# print(type('hello'))
+# print(type(1))
+
+# xx = 1
+# print(type(xx))
+# temp = 98.6
+# print(type(temp))
+# print(type(1))
+# print(type(1.0))
+
+# print(float(99) + 100)
+# i = 42
+# print(type(i))
+# f = float(i)
+# print(f)
+# print(type(f))
+
+#Integer Division
+# print(10 / 2)
+# print(9 / 2)
+# print(99 / 100)
+# print(10.0 / 2.0)
+# print(99.0 / 100)
+
+# *String Conversions
+# sval = '123'
+# print(type(sval))
+# # print(sval + 1) #Error
+# ival = int(sval)
+# print(type(ival))
+# print(ival + 1)
+# nsv = 'hello bob'
+# # niv = int(nsv) #Error
+
+# # * User Input
+# nam = input('Who are you?')
+# print('Welcome', nam)
+
+# * Conberting User input
+inp = input('Europe floor?')
+usf = int(inp) + 1
+print('US floor', usf)
+
