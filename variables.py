7e37e9b5bc72284a8ef871950e6df9a49ad8ac31
Christian MV <chris.mlvz@gmail.com>
2021-02-19 15:51:43 -0600

    Variables, Expressions, and StatementsPassed + gitignore

---
+++ variables.py
@@ -0,0 +1,26 @@
+x = 12.2
+y = 14
+x = 100
+
+x1q3z9ocd = 35.0
+x1q3z9afd = 12.50
+x1q3p9afd = x1q3z9ocd * x1q3z9afd
+print(x1q3p9afd)
+
+a = 35.0
+b = 12.50
+c = a * b
+print(c)
+
+hours = 35.0
+rate = 12.50
+pay = hours * rate
+print(pay)
+
+x = 2
+x = x + 2
+print(x)
+
+x = 0.6
+x = 3.9 * x * (1-x)
+print(x)
