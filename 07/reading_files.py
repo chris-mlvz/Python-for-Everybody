3dc15512a06cb1f1275584fbe93517601dd462fe
Christian MV <chris.mlvz@gmail.com>
2021-02-19 19:40:27 -0600

    Reading Files

---
+++ 07/reading_files.py
@@ -0,0 +1,16 @@
+
+# * Using open()
+# handle = open(filename, mode) #Structure
+
+# * What is a Handle
+# fhand = open('mbox.txt','r') #Exmple
+
+# * When Files are Missing
+# fhand = open('stuff.txt') # ! FileNotFoundError
+
+# * The newline Character
+stuff = 'Hello\nWorld'
+print(stuff)
+stuff = 'X\nY'
+print(stuff)
+len(stuff)
