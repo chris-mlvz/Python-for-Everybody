4d46282d78baf0d4abe8b24c616fa67ea8e211fa
Christian MV <chris.mlvz@gmail.com>
2021-02-19 20:56:04 -0600

    Working with Lists

---
+++ 08/working_with_lists.py
@@ -0,0 +1,47 @@
+
+# * Concatening list using +
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = a + b
+# print(c)
+# print(a)
+
+# * List can be sliced using
+# t = [9, 41, 12, 3, 74, 15]
+# print(t[1:3])
+# print(t[:4])
+# print(t[3:])
+# print(t[:])
+
+# * List Methods
+# x = list()
+# print(type(x))
+# print(dir(x))
+
+# * Building a List from Scracth
+# stuff = list()
+# stuff.append('book')
+# stuff.append(99)
+# print(stuff)
+# stuff.append('cookie')
+# print(stuff)
+
+# * Is Something a List
+# some = [1, 9, 21, 19, 16]
+# print(9 in some)
+# print(15 in some)
+# print(20 not in some)
+
+# * Lists are in Order
+# friends  = ['Joseph', 'Glenn', 'Sally']
+# friends.sort()
+# print(friends)
+# print(friends[1])
+
+# * Built-in Functions and Lists
+nums = [3, 41, 12, 9, 74, 15]
+print(len(nums))
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+print(sum(nums)/(len(nums)))
