e2f6efd409bc19b679981ed6117542a7d69c1719
Christian MV <chris.mlvz@gmail.com>
2021-02-19 19:09:17 -0600

    Strings in Python

---
+++ 06/strings.py
@@ -0,0 +1,71 @@
+
+# * String Data Type
+# str1 = "Hello"
+# str2 = 'there'
+# bob = str1 + str2
+# print(bob)
+
+# str3 = '123'
+# # str3 = str3 + 1 # ! Error
+# x = int(str3) + 1
+# print(x)
+
+# * Reading and Converting
+# name = input('Enter:')
+# print(name)
+# apple = input('Enter:')
+# # x = apple - 10 # ! Error
+# x = int(apple) - 10
+# print(x)
+
+# * Looking Inside Stings
+# fruit = 'banana'
+# letter = fruit[1]
+# print(letter)
+# x = 3
+# w = fruit[x - 1]
+# print(w)
+
+# * A Character Too Far
+# zot = 'abc'
+# print(zot(5)) # ! Error
+
+# * Strings Have Length 
+# fruit = 'banana'
+# print(len(fruit))
+
+# * Len Funcution
+# fruit = 'banana'
+# x = len(fruit)
+# print(x)
+
+# * Looping Through Strings
+# fruit = 'banana'
+# index = 0
+# while index < len(fruit):
+#     letter = fruit[index]
+#     print(index, letter)
+#     index = index + 1
+
+# fruit = 'banana'
+# for letter in fruit:
+#     print(letter)
+
+# index = 0
+# while index < len(fruit):
+#     letter = fruit[index]
+#     print(letter)
+#     index = index + 1
+
+# * Looping and Counting
+# word = 'banana'
+# count = 0
+# for letter in word:
+#     if letter == 'a':
+#         count = count + 1
+# print(count)
+
+# * Looking deeper into in
+
+for letter in 'banana':
+    print(letter)
