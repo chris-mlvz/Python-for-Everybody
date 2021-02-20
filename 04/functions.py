1c32db18be0e090f9461884c9fbd00b3da1b6824
Christian MV <chris.mlvz@gmail.com>
2021-02-19 18:11:00 -0600

    Python Functions

---
+++ 04/functions.py
@@ -0,0 +1,37 @@
+
+# * Function
+# def thing():
+#     print('Hello')
+#     print('Fun')
+
+# thing()
+# print('Zip')
+# thing()
+
+# * Example
+# big = max('Hello world')
+# print(big)
+
+# tiny = min('Hello world')
+# print(tiny)
+
+# * Type Conversions
+# print(float(99) / 100)
+# i = 42
+# print(type(i))
+# f = float(i)
+# print(f)
+# print(type(f))
+# print(1 + 2 * float(3) / 4 - 5)
+
+# * String conversions
+sval = '123'
+print(type(sval))
+# print(sval + 1) #Error
+ival = int(sval)
+print(type(ival))
+print(ival + 1)
+nsv = 'hello bob'
+# niv = int(nsv) #Error
+
+
