b57bd8377da7a6fbfee0bcadca4cb28560520a13
chris-mlvz <chris.mlvz@gmail.com>
2021-02-24 14:46:53 -0600

    Python Dictionaries

---
+++ 09/dictionaries.py
@@ -0,0 +1,24 @@
+
+# * Dictionaries
+# purse = dict()
+# purse['money]'] = 12
+# purse['candy'] = 3
+# purse['tissues'] = 75
+# print(purse)
+# purse['candy'] = purse['candy'] + 2
+# print(purse)
+# print(purse['candy'])
+
+# * Comparing List and Dictionaries
+# ddd = dict()
+# ddd['age'] = 21
+# ddd['course'] = 182
+# print(ddd)
+# ddd['age'] = 23
+# print(ddd)
+
+# * Dictionary Literals(Constants)
+jjj = {'chuck': 1, 'fred': 42, 'jan': 100}
+print(jjj)
+ooo = {}
+print(ooo)
