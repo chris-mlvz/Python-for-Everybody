9eef36328051e71b09cc977ce8ac43313461bbb9
chris-mlvz <chris.mlvz@gmail.com>
2021-02-25 12:33:54 -0600

    Networking: Using urllib in Python

---
+++ 12/using_urllib.py
@@ -0,0 +1,32 @@
+
+# * Using a urllib in Python
+# import urllib.request
+# import urllib.parse
+# import urllib.error
+
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+# for line in fhand:
+#     print(line.decode().strip())
+
+# * Like a file...
+# import urllib.request
+# import urllib.parse
+# import urllib.error
+
+# fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+# counts = dict()
+# for line in fhand:
+#     words = line.decode().split()
+#     for word in words:
+#         counts[word] = counts.get(word, 0) + 1
+# print(counts)
+
+# * Reading Web Pages
+import urllib.request
+import urllib.parse
+import urllib.error
+
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+for line in fhand:
+    print(line.decode().strip())
