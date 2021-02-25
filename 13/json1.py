abcd39d92db2229346485b62f3ecd5a77d85f6a2
chris-mlvz <chris.mlvz@gmail.com>
2021-02-25 14:17:46 -0600

    Web Services: JSON

---
+++ 13/json1.py
@@ -0,0 +1,43 @@
+
+# * Example JSON - Deserialize
+# import json
+# data = '''
+# {
+#     "name": "Chuck",
+#     "phone":
+#     {
+#         "type": "intl",
+#         "number": "+1 734 303 4456"
+#     },
+#     "email":
+#     {
+#         "hide": "yes"
+#     }
+# }
+# '''
+# info = json.loads(data)
+# print('Name:', info["name"])
+# print('Hide:', info["email"]["hide"])
+
+# * More examples
+import json
+data = '''
+[
+    {
+        "id": "001",
+        "x": "2",
+        "name": "Chuck"
+    },
+    {
+        "id": "009",
+        "x": "7",
+        "name": "Chuck"
+    }
+]
+'''
+info = json.loads(data)
+print('User count:', len(info))
+for item in info:
+    print('Name', item['name'])
+    print('Id', item['id'])
+    print('Attribute', item['x'])
