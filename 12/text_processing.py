83b20b9e411c152720aa9e02c80acec107ffc2f8
chris-mlvz <chris.mlvz@gmail.com>
2021-02-24 20:59:43 -0600

    Networking: Text Processing

---
+++ 12/text_processing.py
@@ -0,0 +1,28 @@
+
+# * Representing Simple Strings
+# print(ord('H'))
+# print(ord('e'))
+# print(ord('\n'))
+
+# * Two Kinds of String in Python
+# x = '사랑'
+# print(type(x))
+# x = u'사랑'
+# print(type(x))
+
+# * Python 3
+# x = b'abc'
+# print(type(x))
+# x = '사랑'
+# print(type(x))
+# x = u'사랑'
+# print(type(x))
+
+# * Python Strings to Bytes 
+# ! Example - Don't run
+# while True:
+#     data = mysock.rescv(512)
+#     if(len(data)<1):
+#         break
+#     mystring =data.decode()
+#     print(mystring)
